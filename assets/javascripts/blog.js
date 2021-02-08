e27c8d9d5176ca29fc726c7926953807b38bda08
Flachware <mail@flachware.com>
2021-02-08 15:58:56 +0100

    Fix keyboard focus outline in the header.

--- assets/javascripts/blog.js
+++ assets/javascripts/blog.js
@@ -1,7 +1,9 @@
 'use strict';
 
+import { FocusVisible } from './focus-visible.js';
 import { CategoryNav } from './blog/category-nav.js';
 import { FragmentScroll } from './blog/fragment-scroll.js';
 
+const focusVisible = new FocusVisible();
 const categoryNav = new CategoryNav('.category-nav');
 const fragmentScroll = new FragmentScroll();
