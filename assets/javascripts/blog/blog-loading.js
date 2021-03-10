cbd234d71289be18d68ee10fcc6147416a69d4dd
Flachware <mail@flachware.com>
2021-03-10 12:21:56 +0100

    Fix blog loading script.

--- assets/javascripts/blog/blog-loading.js
+++ assets/javascripts/blog/blog-loading.js
@@ -1,5 +1,8 @@
 if (window.location.hash) {
-  if (performance.navigation.type != performance.navigation.TYPE_RELOAD) {
+  if (
+    performance.navigation.type != performance.navigation.TYPE_RELOAD &&
+    performance.navigation.type != performance.navigation.TYPE_BACK_FORWARD
+  ) {
     const html = document.querySelector('html');
 
     html.classList.add('fragment-loading');
