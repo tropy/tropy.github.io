8c6a92533ff9fa8d4640e42b1f7b192afb631eb6
Flachware <mail@flachware.com>
2021-03-11 14:31:13 +0100

    Refactor fragment loading.

--- assets/javascripts/blog.js
+++ assets/javascripts/blog.js
@@ -1,9 +1,6 @@
-'use strict';
+import { FocusVisible } from './focus-visible.js'
+import { CategoryNav } from './blog/category-nav.js'
 
-import { FocusVisible } from './focus-visible.js';
-import { CategoryNav } from './blog/category-nav.js';
-import { FragmentScroll } from './blog/fragment-scroll.js';
 
-const focusVisible = new FocusVisible();
-const categoryNav = new CategoryNav('.category-nav');
-const fragmentScroll = new FragmentScroll();
+const focusVisible = new FocusVisible()
+const categoryNav = new CategoryNav('.category-nav')
--- assets/javascripts/blog/blog-loading.js
+++ assets/javascripts/blog/blog-loading.js
@@ -1,10 +1,17 @@
-if (window.location.hash) {
-  if (
-    performance.navigation.type != performance.navigation.TYPE_RELOAD &&
-    performance.navigation.type != performance.navigation.TYPE_BACK_FORWARD
-  ) {
-    const html = document.querySelector('html');
+if (
+  window.location.hash &&
+  performance.navigation.type != performance.navigation.TYPE_RELOAD &&
+  performance.navigation.type != performance.navigation.TYPE_BACK_FORWARD
+) {
+  const html = document.querySelector('html')
 
-    html.classList.add('fragment-loading');
+  const removeClasses = function(e) {
+    html.classList.remove('fragment-loading')
+
+    window.removeEventListener('scroll', removeClasses)
   }
+
+  html.classList.add('fragment-loading')
+
+  window.addEventListener('scroll', removeClasses)
 }
