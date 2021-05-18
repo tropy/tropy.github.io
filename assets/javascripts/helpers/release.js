4167cb81c0e2c556691e5ed1ce8161b096c1a304
Sylvester Keil <sylvester@keil.or.at>
2021-05-18 14:20:25 +0200

    Fix release fetcher

--- assets/javascripts/helpers/release.js
+++ assets/javascripts/helpers/release.js
@@ -1,36 +1,13 @@
-import { platform, arch } from './os.js'
-
-const getLatestRelease = () =>
-  fetch('/releases?limit=1')
+export const getLatestRelease = () =>
+  fetch('https://api.tropy.org/releases?limit=1')
     .then(res => res.json())
-    .catch(_ => ({
+    .catch(() => ({
       assets: [
-        { platform: 'darwin', arch: 'x64', url: '/download/mac/x64' },
-        { platform: 'darwin', arch: 'arm64', url: '/download/mac/arm64' },
-        { platform: 'win32', arch: 'x64', url: '/download/windows/x64' },
-        { platform: 'win32', arch: 'arm64', url: '/download/windows/arm64' },
-        { platform: 'win32', arch: 'ia32', url: '/download/windows/ia32' },
-        { platform: 'linux', arch: 'x64', url: '/download/linux/x64' }
+        { platform: 'darwin', arch: 'x64', url: 'https://tropy.org/download/mac/x64' },
+        { platform: 'darwin', arch: 'arm64', url: 'https://tropy.org/download/mac/arm64' },
+        { platform: 'win32', arch: 'x64', url: 'https://tropy.org/download/windows/x64' },
+        { platform: 'win32', arch: 'arm64', url: 'https://tropy.org/download/windows/arm64' },
+        { platform: 'win32', arch: 'ia32', url: 'https://tropy.org/download/windows/ia32' },
+        { platform: 'linux', arch: 'x64', url: 'https://tropy.org/download/linux/x64' }
       ]
     }))
-    .then(release => (
-      release.assets.sort(byPlatform),
-      release
-    ))
-
-
-const byPlatform = (a, b) => {
-  if (a.platform !== platform)
-    return (b.platform === platform) ? 1 : 0
-
-  if (b.platform !== platform) {
-    return -1
-
-  if (a.arch !== arch)
-    return (b.arch === arch) ? 1 : 0
-
-  if (b.arch !== arch)
-    return -1
-
-  return 0
-}
