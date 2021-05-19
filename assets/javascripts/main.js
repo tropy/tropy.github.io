f98ecf41c5ac32042406cb4f827b93bed958bd35
Sylvester Keil <sylvester@keil.or.at>
2021-05-19 15:05:26 +0200

    Add tpy-download to template

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,19 +1,15 @@
 import './elements/dropdown/index.js'
 import './elements/modal/index.js'
 import './elements/dependencies.js'
+import './elements/download.js'
 import './focus-visible.js'
-import { Download } from './elements/download.js'
 import * as os from './helpers/os.js'
 import { getLatestRelease } from './helpers/release.js'
 
 os.guess()
 
 document.addEventListener('DOMContentLoaded', async () => {
-  let release = await getLatestRelease()
-  let button = new Download(release)
-
-  document
-    .querySelector('.hero .container-fluid')
-    .append(button)
+  let button = document.querySelector('tpy-download')
+  button.release = await getLatestRelease()
 
 }, { once: true })
