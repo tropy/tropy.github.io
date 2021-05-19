2cff9e2dfb3b14c1ea4fe43d9ec00bf580fc0119
Flachware <mail@flachware.com>
2021-05-19 16:28:03 +0200

    Remove test version from release.js.

--- assets/javascripts/helpers/release.js
+++ assets/javascripts/helpers/release.js
@@ -2,7 +2,6 @@ export const getLatestRelease = () =>
   fetch('https://api.tropy.org/releases?limit=1')
     .then(res => res.json())
     .catch(() => ({
-      version: '1.9.0',
       assets: [
         { platform: 'darwin', arch: 'x64', url: 'https://tropy.org/download/mac/x64' },
         { platform: 'darwin', arch: 'arm64', url: 'https://tropy.org/download/mac/arm64' },
