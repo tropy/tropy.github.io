c0e966d96f298d153434f59dc77770d9d3b4d22b
Flachware <mail@flachware.com>
2021-09-07 13:19:20 +0200

    Add editable radio.

---
+++ assets/javascripts/elements/editable-radio.js
@@ -0,0 +1,82 @@
+import { createElement } from '../helpers/create-element.js'
+
+
+export const EditableRadio = createElement(
+  'tpy-editable-radio',
+
+  class extends HTMLElement {
+    static get observedAttributes() {
+      return ['is-pristine', 'is-active', 'label', 'type', 'min', 'inputmode',
+        'aria-label']
+    }
+
+    connectedCallback() {
+      this.editable = `
+        <div class="auto-resizer">
+          <div class="currency">$</div>
+          <div class="content"></div>
+          <input
+            type="${this.type}"
+            ${this.min ? `min="${this.min}"` : null}
+            ${this.inputmode ? `inputmode="${this.inputmode}"`: null}
+            required
+            aria-label="${this.ariaLabel}">
+        </div>`
+
+      this.radios = this.parentElement.querySelectorAll('input')
+      this.radio = this.previousElementSibling
+      this.input
+      this.isPristine = true
+
+      this.radio.setAttribute('aria-label', this.label)
+      this.innerText = this.label
+
+      this.radios.forEach(radio => {
+        radio.addEventListener('change', this.handleChange)
+      })
+
+      this.addEventListener('click', this.activate)
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+      if (name == 'is-active') {
+        this.isActive ? this.activate() : this.deactivate()
+      }
+    }
+
+    handleChange = (e) => {
+      this.radio.checked ? this.isActive = true : this.isActive = false
+    }
+
+    activate = () => {
+      if (this.isPristine) {
+        this.radio.click()
+        this.innerHTML = this.editable
+        this.input = this.querySelector('input')
+        this.isPristine = false
+      }
+
+      this.input.focus()
+
+      this.input.addEventListener('input', this.update)
+    }
+
+    deactivate = () => {
+      this.innerText = this.label
+      this.isPristine = true
+    }
+
+    update = (e) => {
+      this.radio.value = this.input.value
+      this.querySelector('.content').innerText = this.input.value
+    }
+  }, {
+    isPristine: true,
+    isActive: true,
+    label: false,
+    type: false,
+    min: false,
+    inputmode: false,
+    ariaLabel: false
+  }
+)
