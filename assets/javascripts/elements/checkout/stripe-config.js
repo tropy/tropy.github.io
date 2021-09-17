c21c749cc6db68566d5e056f71f513fa5cc2afc9
Flachware <mail@flachware.com>
2021-09-17 22:10:51 +0200

    Allow multiple loading scripts per page.

--- assets/javascripts/elements/checkout/stripe-config.js
+++ assets/javascripts/elements/checkout/stripe-config.js
@@ -2,14 +2,14 @@
 ---
 
 {% if jekyll.environment == "development" %}
-  export var stripe = Stripe("{{ site.stripe_dev }}");
-  export const paymentEndpoint = "{{ site.paymentEndpoint_dev }}/start-payment";
+  var stripe = Stripe("{{ site.stripe_dev }}");
+  const paymentEndpoint = "{{ site.paymentEndpoint_dev }}/start-payment";
 {% elsif jekyll.environment == "production" %}
-  export var stripe = Stripe("{{ site.stripe }}");
-  export const paymentEndpoint = "{{ site.paymentEndpoint }}/start-payment";
+  var stripe = Stripe("{{ site.stripe }}");
+  const paymentEndpoint = "{{ site.paymentEndpoint }}/start-payment";
 {% endif %}
 
-export const style = {
+const style = {
   base: {
     color: "#101012",
     fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif',
