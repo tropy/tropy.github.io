4ba9617ec2133b30faef7ae60bea713e03ecc0f8
Flachware <mail@flachware.com>
2021-11-18 00:59:55 +0100

    Enable catch block automatically in development environment.

--- assets/javascripts/helpers/release.js
+++ assets/javascripts/helpers/release.js
@@ -1,13 +1,17 @@
+---
+---
 export const getLatestRelease = () =>
   fetch('/releases/latest?limit=1')
     .then(res => res.json())
     .then(res => res[0])
-    //.catch(() => ({
-    //  url: 'https://github.com/tropy/tropy/releases/latest',
-    //  assets: [
-    //    { platform: 'darwin', arch: 'x64', url: 'https://tropy.org/download/mac/x64' },
-    //    { platform: 'darwin', arch: 'arm64', url: 'https://tropy.org/download/mac/arm64' },
-    //    { platform: 'win32', arch: 'x64', url: 'https://tropy.org/download/windows/x64' },
-    //    { platform: 'linux', arch: 'x64', url: 'https://tropy.org/download/linux/x64' }
-    //  ]
-    //}))
+    {%- if jekyll.environment == "development" %}
+    .catch(() => ({
+      url: 'https://github.com/tropy/tropy/releases/latest',
+      assets: [
+        { platform: 'darwin', arch: 'x64', url: 'https://tropy.org/download/mac/x64' },
+        { platform: 'darwin', arch: 'arm64', url: 'https://tropy.org/download/mac/arm64' },
+        { platform: 'win32', arch: 'x64', url: 'https://tropy.org/download/windows/x64' },
+        { platform: 'linux', arch: 'x64', url: 'https://tropy.org/download/linux/x64' }
+      ]
+    }))
+    {%- endif %}
