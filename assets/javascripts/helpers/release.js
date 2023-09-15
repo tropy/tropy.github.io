a909bf1cdd252af667e6d24372ad5bf8ba64a39f
Flachware <mail@flachware.com>
2023-09-15 19:13:54 +0200

    Remove catch block with dummy object

--- assets/javascripts/helpers/release.js
+++ assets/javascripts/helpers/release.js
@@ -4,14 +4,3 @@ export const getLatestRelease = () =>
   fetch('https://tropy.org/releases/latest?limit=1')
     .then(res => res.json())
     .then(res => res[0])
-    {%- if jekyll.environment == "development" %}
-    .catch(() => ({
-      url: 'https://github.com/tropy/tropy/releases/latest',
-      assets: [
-        { platform: 'darwin', arch: 'x64', url: 'https://tropy.org/download/mac/x64' },
-        { platform: 'darwin', arch: 'arm64', url: 'https://tropy.org/download/mac/arm64' },
-        { platform: 'win32', arch: 'x64', url: 'https://tropy.org/download/windows/x64' },
-        { platform: 'linux', arch: 'x64', url: 'https://tropy.org/download/linux/x64' }
-      ]
-    }))
-    {%- endif %}
